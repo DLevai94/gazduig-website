9d13d208f193be05d49268bcdce63838a6c28f55
David Levai <david@davidlevai.com>
2020-07-22 06:14:18 +0200

    cloudinary test

--- src/templates/workTemplate.js
+++ src/templates/workTemplate.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
-import Img from 'gatsby-image';
 import CrownIcon from '../components/svg/crown';
 import CalendarIcon from '../components/svg/calendar';
 import KeyIcon from '../components/svg/key';
@@ -11,6 +10,7 @@ import Contact from '../components/contact-form';
 export default function Template({ data }) {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
+  console.log(frontmatter);
   return (
     <Layout>
       <SEO
@@ -70,11 +70,7 @@ export default function Template({ data }) {
           </div>
         </div>
         <div className="md:px-16 mt-16 mb-32">
-          <Img
-            className="w-100 rounded mb-16"
-            fluid={frontmatter?.images?.childImageSharp?.fluid}
-            alt={frontmatter?.title}
-          />
+          <img src={frontmatter?.images} alt={frontmatter?.title} className="w-100 rounded" />
         </div>
 
         <section className="bg-white md:rounded md:shadow-brand py-8 px-8 md:px-36 z-50">
