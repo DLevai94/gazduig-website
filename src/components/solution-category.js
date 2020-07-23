5ef2d4416adfcd5bd3f8b5f9fec7e817c119cc4c
David Levai <david@davidlevai.com>
2020-07-23 16:10:42 +0200

    css fix

--- src/components/solution-category.js
+++ src/components/solution-category.js
@@ -13,7 +13,7 @@ const SolutionCategory = ({ excerpt, description, children, name }) => {
       <h2 className="max-w-4xl font-display text-2xl font-extrabold mb-4">Így hajthat hasznot számodra:</h2>
       <p className="max-w-4xl font-light text-xl mb-28" dangerouslySetInnerHTML={{ __html: description }}></p>
       <h2 className="max-w-4xl font-display text-2xl font-extrabold">{` ${name} közben általában ezeket csináljuk`}</h2>
-      <div className="w-full mx-auto grid md:grid-cols-2 lg:grid-cols-3 col-gap-6 row-gap-3 mt-8 mb-16">{children}</div>
+      <div className="w-full mx-auto grid md:grid-cols-2 lg:grid-cols-3 col-gap-6 row-gap-6 mt-8 mb-16">{children}</div>
     </div>
   );
 };
