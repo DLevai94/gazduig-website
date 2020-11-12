16e4c97b2601fa51058ee75f9b37e8d93e603e6a
Roland Levai <70857984+aegnorwynn219@users.noreply.github.com>
2020-11-12 13:36:49 +0100

    Trying out git

--- src/templates/workTemplate.js
+++ src/templates/workTemplate.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { graphql, Link } from 'gatsby';
-import CrownIcon from '../components/svg/crown';
-import CalendarIcon from '../components/svg/calendar';
-import KeyIcon from '../components/svg/key';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import Contact from '../components/contact-form';
+import React from "react";
+import { graphql, Link } from "gatsby";
+import CrownIcon from "../components/svg/crown";
+import CalendarIcon from "../components/svg/calendar";
+import KeyIcon from "../components/svg/key";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Contact from "../components/contact-form";
 
 export default function Template({ data }) {
   const { markdownRemark } = data;
@@ -15,16 +15,16 @@ export default function Template({ data }) {
       <SEO
         title={frontmatter?.title}
         keywords={[
-          'arculattervezés budapest',
-          'arculattervezés',
-          'webdizájn budapest',
-          'webdizájn',
-          'kreatív ügynökség budapest',
-          'budapest creative agency',
-          'instagram márkaépítés',
-          'insta márkaépítés',
-          'ütős',
-          'gazduig',
+          "arculattervezés budapest",
+          "arculattervezés",
+          "webdizájn budapest",
+          "webdizájn",
+          "kreatív ügynökség budapest",
+          "budapest creative agency",
+          "instagram márkaépítés",
+          "insta márkaépítés",
+          "ütős",
+          "gazduig",
         ]}
       />
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -42,8 +42,10 @@ export default function Template({ data }) {
                 <CrownIcon className="w-8 mr-3" />
               </div>
               <div className="flex flex-col justify-center items-start">
-                <p className="font-light text-xs">Ügyfél</p>
-                <p className="font-extrabold text-sm leading-tight">{frontmatter?.client}</p>
+                <p className="font-light text-xs">Ugyfel</p>
+                <p className="font-extrabold text-sm leading-tight">
+                  {frontmatter?.client}
+                </p>
               </div>
             </div>
             <div className="md:rounded md:bg-white mb-4 md:mb-12 flex flex-row p-4 md:shadow-brand justify-start items-center">
@@ -52,7 +54,9 @@ export default function Template({ data }) {
               </div>
               <div className="flex flex-col justify-center items-start">
                 <p className="font-light text-xs">Megoldások</p>
-                <p className="font-extrabold text-sm leading-tight">{frontmatter?.solutions}</p>
+                <p className="font-extrabold text-sm leading-tight">
+                  {frontmatter?.solutions}
+                </p>
               </div>
             </div>
             <div className="md:rounded md:bg-white mb-4 md:mb-12 flex flex-row p-4 md:shadow-brand justify-start items-center">
@@ -61,18 +65,28 @@ export default function Template({ data }) {
               </div>
               <div className="flex flex-col justify-center items-start">
                 <p className="font-light text-xs">Ekkor készült</p>
-                <p className="font-extrabold text-sm leading-tight">{frontmatter?.jobtime}</p>
+                <p className="font-extrabold text-sm leading-tight">
+                  {frontmatter?.jobtime}
+                </p>
               </div>
             </div>
           </div>
           <div className="col-span-6">
             <p className="font-extrabold">Egy rövid összefoglaló</p>
-            <div className="mt-8 font-light" dangerouslySetInnerHTML={{ __html: html }} />
+            <div
+              className="mt-8 font-light"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
           </div>
         </div>
         <div className="md:px-16 mt-16 mb-32">
           {frontmatter?.images?.map((image) => (
-            <img key={image} src={image} alt={frontmatter?.title} className="w-100 rounded mx-auto mb-8" />
+            <img
+              key={image}
+              src={image}
+              alt={frontmatter?.title}
+              className="w-100 rounded mx-auto mb-8"
+            />
           ))}
         </div>
 
