0f8631f2e097645fb91b2c5d323ae781fff2c6e6
Roland Levai <70857984+aegnorwynn219@users.noreply.github.com>
2020-12-02 13:34:43 +0100

    Changes

--- src/components/solution-category.js
+++ src/components/solution-category.js
@@ -4,22 +4,27 @@ const SolutionCategory = ({ excerpt, description, children, name, fullName }) =>
   return (
     <div className="bg-gray-100 mb-32">
       <div
-        className="leading-snug md:px-32 md:py-16"
+        className="leading-snug md:px-32 md:py-16 flex"
         // style={{
         //   background:
         //     'linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 40%, rgba(247,247,247,1) 41%, rgba(247,247,247,1) 100%)',
         // }}
       >
-        <h2 className="font-black text-3xl">{`${name}`}</h2>
-        <h3 className="max-w-4xl font-display text-2xl font-extrabold mb-4 leading-brand">
-          Így hajthat hasznot számodra
-        </h3>
-        <p className="max-w-4xl font-light text-lg mb-28" dangerouslySetInnerHTML={{ __html: description }}></p>
-        {fullName ? (
-          <h3 className="max-w-4xl font-display text-2xl font-extrabold leading-brand">{` ${fullName}`}</h3>
-        ) : null}
-        <h2 className="max-w-4xl font-display text-2xl font-extrabold mb-4 leading-brand">Röviden</h2>
-        <p className="max-w-4xl font-light text-lg mb-12">{excerpt}</p>
+        <h2 className="font-black text-3xl block w-full">{`${name}`}</h2>
+        <div>
+          <h3 className="max-w-4xl font-display text-2xl font-extrabold mb-4 leading-brand">
+            Így hajthat hasznot számodra
+          </h3>
+          <p className="max-w-4xl font-light text-lg mb-28" dangerouslySetInnerHTML={{ __html: description }}></p>
+          {fullName ? (
+            <h3 className="max-w-4xl font-display text-2xl font-extrabold leading-brand">{` ${fullName}`}</h3>
+          ) : null}
+        </div>
+        <div>
+          <h2 className="max-w-4xl font-display text-2xl font-extrabold mb-4 leading-brand">Röviden</h2>
+          <p className="max-w-4xl font-light text-lg mb-12">{excerpt}</p>
+        </div>
+
         {name ? (
           <h3 className="max-w-4xl font-display text-2xl font-extrabold leading-brand">{` Ilyenkor általában ezeket csináljuk`}</h3>
         ) : null}
